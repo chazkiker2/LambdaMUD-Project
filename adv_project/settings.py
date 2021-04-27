ab6d01361f79283d8ec1c4f8bd280eb08c93eadb
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-27 12:04:04 -0400

    set up webpack bundler and hybrid Django-React

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'adv_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,6 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
