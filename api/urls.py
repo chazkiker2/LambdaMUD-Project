19df6a69f411348642bc51d5d8a4fb3170cf0a87
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-26 23:25:34 -0400

    mess around with login endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,32 @@
 from django.urls import include, path
-from django.conf.urls import url
+from django.contrib.auth import login, authenticate
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+import json
+
+
+@csrf_exempt
+@api_view(http_method_names=["POST"])
+def mud_login(request):
+    user_info = json.loads(request.body)
+    username = user_info['username']
+    password = user_info['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return JsonResponse(
+            status=200,
+            data={**user},
+            safe=True
+        )
+    else:
+        return HttpResponse(status=404)
+
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
+    path("api-login", mud_login, name="api-login"),
     path('registration/', include('rest_auth.registration.urls')),
 ]
