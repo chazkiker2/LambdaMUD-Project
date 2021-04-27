20bf37fb241e3bf957ecc098785bb8c08d9688bc
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-27 19:42:15 -0400

    successfully able to log in with coreapi and CSRF

--- assets/utils/index.js
+++ assets/utils/index.js
@@ -1,12 +1,8 @@
 import axios from "axios"
+
 const API_BASE_URL = "http://127.0.0.1:8000"
 
-// const URLS = {
-//     LOGIN: '{% url 'api-login' %}',
-//     REGISTER: "{% url 'api-register' %}",
-// }
-const URLS = JSON.parse(document.getElementById("urls").textContent)
-console.log({ URLS })
+
 const axiosAuth = function () {
     const token = window.localStorage.getItem("key")
 
@@ -18,24 +14,31 @@ const axiosAuth = function () {
     })
 }
 
-// function loginUser({ username, password }) {
-//     console.log({ coreapi, schema })
-//     let action = ["api-token-auth", "obtain-token"]
-//     let params = { username, password }
-//     client
-//         .action(window.schema, action, params)
-//         .then(res => {
-//             console.log({ res })
-//             let auth = window.coreapi.auth.TokenAuthentication({
-//                 scheme: "JWT",
-//                 token: res["token"],
-//             })
-//             window.localStorage.setItem("key", res["token"])
-//             window.client = coreapi.Client({ auth })
-//             window.loggedIn = true
-//         })
-//         .catch(err => console.log({ err }))
-// }
+window.auth = new window.coreapi.auth.SessionAuthentication({
+    csrfCookieName: 'csrftoken',
+    csrfHeaderName: 'X-CSRFToken'
+});
+window.client = new window.coreapi.Client({auth: window.auth});
+window.loggedIn = false;
+
+
+function loginUser({username, password}) {
+    let action = ["api-token-auth", "obtain-token"]
+    let params = {username, password}
+    window.client
+        .action(window.schema, ["api", "api-login", "create"], params)
+        .then(res => {
+            console.log({res})
+            let auth = window.coreapi.auth.SessionAuthentication({
+                csrfCookieName: 'csrftoken',
+                csrfHeaderName: 'X-CSRFToken'
+            });
+            window.localStorage.setItem("key", res["token"])
+            window.client = coreapi.Client({auth})
+            window.loggedIn = true
+        })
+        .catch(err => console.log({err}))
+}
 
 async function post(url = "", data = {}) {
     const res = await fetch(url, {
@@ -56,17 +59,17 @@ async function post(url = "", data = {}) {
 
 export const api = {
     API_BASE_URL,
-    ...URLS,
-    axios: axios.create({ baseURL: API_BASE_URL }),
+    // ...URLS,
+    axios: axios.create({baseURL: API_BASE_URL}),
     axiosAuth,
-    login: userInfo => axios.post(`${API_BASE_URL}/api/api-login`, userInfo),
+    // login: userInfo => axios.post(`${API_BASE_URL}/api/api-login`, userInfo),
     // login: userInfo => post(URLS.LOGIN, userInfo),
-    // login: userInfo => loginUser(userInfo),
+    login: userInfo => loginUser(userInfo),
     register: userInfo =>
         axios.post(`${API_BASE_URL}/api/api-register`, userInfo),
     initialize: () => axiosAuth().get("/api/adv/init"),
-    move: direction => axiosAuth().post("/api/adv/move", { direction }),
-    say: message => axiosAuth().post("/api/adv/say", { message }),
+    move: direction => axiosAuth().post("/api/adv/move", {direction}),
+    say: message => axiosAuth().post("/api/adv/say", {message}),
 }
 
 export default api
