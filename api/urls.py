dbd6fad9546b9992d6bdcfca511725d0d7107aca
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-28 18:53:26 -0400

    try '@csrf_exempt' decorator on login view

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@ from rest_auth.views import LoginView
 from rest_auth.registration.views import RegisterView
 from django.urls import path
 from django.conf.urls import include, url
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     # path('', include('rest_auth.urls')),
     # path('registration/', include('rest_auth.registration.urls')),
-    path("api-login/", LoginView.as_view(), name="api-login"),
-    path("api-register/", RegisterView.as_view(), name="api-register"),
+    path("api-login/", csrf_exempt(LoginView.as_view()), name="api-login"),
+    path("api-register/", csrf_exempt(RegisterView.as_view()), name="api-register"),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
