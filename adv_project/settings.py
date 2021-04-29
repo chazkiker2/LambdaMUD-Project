73d971b93de26ce2007df7529e769fa9e899b246
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-29 19:03:59 -0400

    static files settings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -154,11 +154,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)  # new
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # new
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'  # new
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: "./assets/index.js",  // path to our input file
     output: {
         filename: "bundle.js",  // output bundle file name
-        path: path.resolve(__dirname, "./static"),  // path to our Django static directory
+        // path: path.resolve(__dirname, "./static"),  // path to our Django static directory
+        path: path.resolve(__dirname, "static")
     },
     plugins,
     module: {
